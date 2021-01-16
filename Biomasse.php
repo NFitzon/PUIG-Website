98e66b359eaca66164074ec3562db711b795a45d
ChewieTheBaca <nikolaifitzon@posteo.net>
2021-01-16 21:08:19 +0000

    init

---
+++ Biomasse.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <!-- Header -->
+    <?php include("header.php"); ?>
+    <h1>Biomasse</h1>
+    <p>
+         Bioenergie wird aus dem Rohstoff Biomasse gewonnen. Biomasse ist gespeicherte Sonnenenergie in Form von Energiepflanzen, Holz oder Reststoffen 
+         wie etwa Stroh, Biomüll oder Gülle. Bioenergie ist unter den Erneuerbaren Energieträgern der „Alleskönner“: 
+         Sowohl Strom, Wärme als auch Treibstoffe können aus fester, flüssiger und gasförmiger Biomasse gewonnen werden.
+         <br>
+    </p>
+    <!-- Footer -->
+    <?php include("footer.php"); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css" />
+	<title>EEAkzep</title>
+</head>
+
+<header>
+			<div id="haupttitel">
+				<h1 class="titel">EEAkzep</h1>
+				<h2 class="slogan" style="color: #4cba78;">Weil wir unsere Natur lieben</h2>
+			</div>
+
+			<div class="ersterNav">
+				<nav>
+					<ul>
+						<li><a href="#">Startseite</a></li>
+						<li><a href="#">Über Uns</a></li>
+						<li><a href="#">Kontakt</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="zweiteNav">
+				<nav>
+					<ul>
+						<li>
+							<div class="dropdown">
+								<a href="ee.php" class="dropbtn">Erneuerbare Energien</a>
+								<div class="dropdown-content">
+									<a href="#">Wind</a>
+									<a href="#">Sonne</a>
+									<a href="#">Biomasse</a>
+									<a href="#">Wasser</a>
+								</div>
+							</div>
+						</li>
+						<li><a href="#">Handlungsempfehlungen</a></li>
+						<li><a href="#">Warum Erneuerbare Energien?</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
